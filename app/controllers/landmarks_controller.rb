45792021b6bc0c1e226bb0578234c7a616e96555
Sean Oughton <seanoughton@gmail.com>
2018-04-06 22:44:19 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,11 +1,15 @@
 class LandmarksController < ApplicationController
 
+
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
   get '/landmarks/new' do
     erb :'/landmarks/new'
   end
 
   post '/landmarks/new' do
-    binding.pry
     @new_landmark = Landmark.create(params[:landmark])
     @new_landmark.save
   end

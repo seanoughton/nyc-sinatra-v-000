ce5ae8e2831f4fdaaf3c4c1868babfb243343302
Sean Oughton <seanoughton@gmail.com>
2018-04-06 18:23:20 +0000

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,9 +1,10 @@
 class CreateLandmarks < ActiveRecord::Migration[4.2]
   #raise "Write CreateLandmarks migration here"
-  create_table :landmarks do |t|
-    t.string :name
-    t.integer :figure_id
-    t.integer :year_completed
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.integer :year_completed
+    end
   end
-
 end

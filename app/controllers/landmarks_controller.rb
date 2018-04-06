3cee67c3bf30daca1029f7fbb7de9188ef174a02
Sean Oughton <seanoughton@gmail.com>
2018-04-06 23:09:47 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -25,7 +25,6 @@ class LandmarksController < ApplicationController
   end
 
   patch '/landmarks/:id' do
-    binding.pry
     @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
     @landmark.save

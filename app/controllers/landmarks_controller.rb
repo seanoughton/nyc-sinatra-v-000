9b8fdb4b7b6b86cc7b5688aef815e41844734fd1
Sean Oughton <seanoughton@gmail.com>
2018-04-06 22:28:07 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,13 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks/new' do
+    #@new_landmark = Figure.create(params[:landmark])
+    #@new_landmark.save
+  end
+
+
 end

16c03f8229667fa4588c9b7620d98f0904b9e5cf
Sean Oughton <seanoughton@gmail.com>
2018-04-06 21:17:03 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,6 +8,8 @@ class FiguresController < ApplicationController
     @new_figure = Figure.create(params[:figure])
     @new_title = Title.create(params[:title])
     @new_figure.titles << @new_title
+    @new_landmark = Title.create(params[:landmark])
+    @new_figure.landmarks << @new_landmark
     @new_figure.save
   end
 
